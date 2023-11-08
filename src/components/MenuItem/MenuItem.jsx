5ce573e978dd573ae3c461b101fcb98f5d2bdc04
MiloszPlesniak <105582642+MiloszPlesniak@users.noreply.github.com>
2023-11-08 01:54:14 +0100

    open menu handling

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,23 +1,47 @@
 import { Link } from 'react-router-dom';
 import { nanoid } from 'nanoid';
+import clsx from 'clsx';
+import { useState } from 'react';
 import style from './MenuItem.module.scss';
 import { BsArrowBarRight } from 'react-icons/bs';
 import Media from 'react-media';
 
 const MenuItem = ({ title, table }) => {
+  const openMenuItem = {
+    menu: clsx(style.menuItem, style['menuItem--open']),
+    arrowBtn: clsx(style.menuShowBtn, style['menuShowBtn--open']),
+  };
+  const closeMenuItem = {
+    menu: clsx(style.menuItem, style['menuItem--close']),
+    arrowBtn: clsx(style.menuShowBtn, style['menuShowBtn--close']),
+  };
+
+  const [togleMenu, setTogleMenu] = useState(openMenuItem.menu);
+  const [togleBtn, setTogleBtn] = useState(openMenuItem.arrowBtn);
   const openList = e => {
     e.target.nextSibling.classList.toggle(style.show);
   };
+  const openMenu = () => {
+    if (
+      togleMenu === 'MenuItem_menuItem__Dra2C MenuItem_menuItem--open__8T0UX'
+    ) {
+      setTogleMenu(closeMenuItem.menu);
+      setTogleBtn(closeMenuItem.arrowBtn);
+    } else {
+      setTogleMenu(openMenuItem.menu);
+      setTogleBtn(openMenuItem.arrowBtn);
+    }
+  };
 
   return (
-    <div className={style.menuItem}>
+    <div className={togleMenu}>
       <Media
         query="(max-width:800px)"
         render={() => (
-          <BsArrowBarRight className={style.menuShowBtn}/>
+          <BsArrowBarRight onClick={openMenu} className={togleBtn} />
         )}
       />
-      
+
       <p onClick={openList} className={style.menuItem__header}>
         {title}
         <span className={style.menuItem__icon}></span>
